40471d16561b8371cbeb8fc743bb401b1a65e5d9
Keshav R Karanth <48642293+keshavrkaranth@users.noreply.github.com>
2022-05-11 14:05:34 +0530

    minor changes

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Account,Room,Hostel,Warden,Leave
+from .models import Account, Room, Hostel, Warden, Leave
+from django.contrib.auth.admin import UserAdmin
+
+
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ('name', 'username', 'email')
+    exclude = ['date_joined','last_login','last_name']
+
+
+admin.site.register(Account, AccountAdmin)
 admin.site.register(Room)
 admin.site.register(Hostel)
 admin.site.register(Warden)
--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
+import django_filters
+from django_filters import FilterSet
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, ChoiceFilter
 from rest_framework import filters
 
 from . import serializers
@@ -16,13 +18,24 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = serializers.MyTokenObtainPairSerializer
 
 
+class MyPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
+        else:
+            return True
+
+
 class RoomViewSet(ModelViewSet):
     queryset = Room.objects.all().order_by('-created_at')
     serializer_class = serializers.RoomsSerializer
     pagination_class = PageNumberPagination
-    filter_backends = [DjangoFilterBackend,filters.OrderingFilter]
-    filterset_fields = ['vacant','hostel']
+    permission_classes = [MyPermission]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['vacant', 'hostel__name', 'room_type']
     ordering_fields = ['created_at']
+
+
 
 
 @api_view(['GET'])
